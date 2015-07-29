16a6c4e9a2994c4d94e6726c88c417191a571b43
Askarbek Karasasev <askar.karasaev@gmail.com>
2015-07-29 19:12:25 +0900

    Comments can now be edited

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -20,6 +20,19 @@ class CommentsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      flash[:notice] = 'Comment has been updated.'
+      redirect_to [@item, @comment]
+    else
+      flash.now[:alert] = 'Comment has not been updated.'
+      render 'edit'
+    end
+  end
+
   private
 
   def comment_params
--- spec/factories/item_factory.rb
+++ spec/factories/item_factory.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :item do
     name 'Example item'
+    quantity 3
   end
 end
