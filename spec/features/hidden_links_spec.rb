a5b9ae78f05e9642d2d72782e4b98a06a11996e5
Askarbek Karasaev <askar.karasaev@gmail.com>
2015-07-31 22:42:50 +0900

    Hide the New Item link from anonymous users

---
+++ spec/features/hidden_links_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature 'Users can only see the appropriate links' do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:admin) { FactoryGirl.create(:user, :admin) }
+
+  context 'anonymous users' do
+    scenario 'cannot see the New Item link' do
+      visit '/'
+      expect(page).not_to have_link 'New Item'
+    end
+  end
+
+  context 'regular users' do
+    before { login_as(user) }
+
+    scenario 'can see the New Item link' do
+      visit '/'
+      expect(page).to have_link 'New Item'
+    end
+  end
+
+  context 'admin users' do
+    before { login_as(admin) }
+
+    scenario 'can see the New Item link' do
+      visit '/'
+      expect(page).to have_link 'New Item'
+    end
+  end
+
+
+end
