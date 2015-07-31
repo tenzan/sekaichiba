b64302fe9df139ef964b4a61feae7dfce8dc26c7
Askarbek Karasasev <askar.karasaev@gmail.com>
2015-07-31 11:12:49 +0900

    Added admin flag to User model, and seeded the first admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless User.exists?(email: 'admin@example.com')
+  User.create!(email: 'admin@example.com', password: 'password', admin: true)
+end
+
+unless User.exists?(email: 'staff@example.com')
+  User.create!(email: 'staff@example.com', password: 'password')
+end
+
+['Shampoo', 'Conditioner'].each do |name|
+  unless Item.exists?(name: name)
+    Item.create!(name: name, quantity: 1)
+  end
+end
--- spec/features/creating_items_spec.rb
+++ spec/features/creating_items_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'Users can create new items' do
   before do
+    login_as(FactoryGirl.create(:user, :admin))
     visit '/'
 
     click_link 'New Item'
